9f2f0a998f8004c3032e309b9eed603bc447077d
Roman Moskalenko <2016roman00@gmail.com>
2024-08-23 08:35:14 +0300

    initial commit

---
+++ src/main/java/com/romanm/multithreadingromanm/MultithreadingRomanmApplication.java
@@ -0,0 +1,9 @@
+package com.romanm.multithreadingromanm;
+
+
+public class MultithreadingRomanmApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/com/romanm/multithreadingromanm/MultithreadingRomanmApplicationTests.java
@@ -0,0 +1,11 @@
+package com.romanm.multithreadingromanm;
+
+import org.junit.jupiter.api.Test;
+
+class MultithreadingRomanmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
